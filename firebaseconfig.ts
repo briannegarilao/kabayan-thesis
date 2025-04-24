43c2884a6124728f4925feac0e5fe4dd93730d41
Brianne Garilao <briannegarilao@gmail.com>
2025-04-24 15:08:34 +0800

    Change in firebaseconfig

--- firebaseconfig.ts
+++ firebaseconfig.ts
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { collection, getDocs } from "firebase/firestore";
 
@@ -16,7 +15,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 // Reference to Firestore collections
